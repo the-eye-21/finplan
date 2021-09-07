b1a6ba08f2d15d4635288a3f9de38da8ef590f1f
Lochan Manthena <lochman07@gmail.com>
2021-09-07 16:15:02 +0530

    Chart Change

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../index.css';
-import Plot from 'react-plotly.js';
+
+import {Line} from 'react-chartjs-2';
 
 class Chart extends React.Component{
   constructor(props){
@@ -19,25 +20,95 @@ class Chart extends React.Component{
     // totMoney=this.props.yData2;
     // invMoney=this.props.yData3;
     //
+
+    let chartData={
+      labels:this.props.xData,
+      datasets:[
+        {
+          type:'line',
+          label:"Total Money",
+          data: this.props.yData2,
+          borderColor: 'rgb(75, 192, 192)',
+          yAxisID :'tot'
+
+        },
+        {
+          type:'bar',
+          label:"Annual Spending",
+          data:this.props.yData1,
+          backgroundColor: 'rgba(255, 158, 3,0.4)',
+          yAxisID :'spend'
+        }
+      ]
+    }
     return(
       <div>
 
       <div id="tester" className="tester">
-      <Plot
-        data={[
-          {
-            x: this.props.xData,
-            y: this.props.yData2,
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-            name: 'Total Money'
+      <Line
+      id="chart"
+      data={chartData}
+      options={{
+        maintainAspectRatio:false,
+        animation:{
+          duration:0,
+        },
+        plugins: {
+
+          title: {
+            display: true,
+            text: "Your Money"
           },
-        {type: 'bar', x: this.props.xData, y:this.props.yData1, name:'Your Planned Spending'}
-        ]}
-        layout={ {width: 1000, height: 800, title: 'Your Money'} }
+          legend: {
+            display: true,
+            position: "bottom"
+         }
+       },
+       stacked:false,
+       scales:{
+
+         tot:{
+           type:'linear',
+           display:'true',
+           position:'left',
+           grid:{
+             display:true,
+           },
+           ticks:{
+             color:'rgb(75, 192, 192)',
+             callback: function(value,index, values){
+               if(value>=10000000){return value/10000000 + ' Crore';}
+               else if(value>100000){return value/100000 + ' Lakh';}
+               else {return value;}
+             }
+           }
+         },
+         spend:{
+           type:'linear',
+           display:'true',
+           position:'right',
+           grid:{
+             display:false,
+           },
+
+           ticks:{
+             color:'rgba(255, 158, 3,1.0)',
+             callback: function(value,index, values){
+               if(value>=10000000){return value/10000000 + ' Crore';}
+               else if(value>100000){return value/100000 + ' Lakh';}
+               else {return value;}
+             }
+           }
+         }
+
+       }
+
+
+      }}
+
       />
       </div>
+
       </div>
     )
   }
